f61db68c83e83a6c5d0dfe8c6f083cc0d12c9e69
David Stein <stein.rs@gmail.com>
2022-06-28 16:20:45 -0300

    ajuste

--- forca.py
+++ forca.py
@@ -11,7 +11,7 @@ def jogar_forca():
     arquivo = open('palavras.txt','r') # abra o arquivo.tx com as palavras a ser escolhidas
 
     for x in arquivo: # peca o conteudo do arquivo e coloca dentro da palavraSortida
-        palavraSortida.append(x.strip())# elimina os espaços
+        palavraSortida.append(x.strip())# elimina os espaços e adiciona na lista
     arquivo.close()# fecha a leitura do arquivo
 
     posicao = random.randrange(0,len(palavraSortida))# gera um numero aleatório com base na quantidade de palavras
