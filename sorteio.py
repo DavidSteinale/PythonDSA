787fd3954c58e61a344c1ff59714efb8bae1b3ad
David Stein <stein.rs@gmail.com>
2022-06-28 16:56:38 -0300

    ajuste

---
+++ sorteio.py
@@ -0,0 +1,15 @@
+from random import random
+
+def sortear():
+    arquivo = open('palavras.txt', 'r')  # abra o arquivo.tx com as palavras a ser escolhidas
+    palavraSortida = []  # lista para receber uma palavra soteada
+
+    for x in arquivo:  # peca o conteudo do arquivo e coloca dentro da palavraSortida
+        palavraSortida.append(x.strip())  # elimina os espaços e adiciona na lista
+    arquivo.close()  # fecha a leitura do arquivo
+
+    posicao = random.randrange(0, len(palavraSortida))  # gera um numero aleatório com base na quantidade de palavras
+    print(palavraSortida[posicao])
+
+if(__name__ == "__main__"):
+    sortear()
