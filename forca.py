9dfd3c88c22ed3533fa9fd7657d7e5f1b9b0775c
David Stein <stein.rs@gmail.com>
2022-06-28 17:04:13 -0300

    ajuste

--- forca.py
+++ forca.py
@@ -1,31 +1,26 @@
 import random
 
+import sorteio
+
+
 def jogar_forca():
     print('ola ao jogo forca')
-    palavraSortida = [] # lista para receber uma palavra soteada
+  #  palavraSortida = [] # lista para receber uma palavra soteada
     acerto = 0
     errou = 0
     enforcou = False
     acertou = False
 
-    arquivo = open('palavras.txt','r') # abra o arquivo.tx com as palavras a ser escolhidas
-
-    for x in arquivo: # peca o conteudo do arquivo e coloca dentro da palavraSortida
-        palavraSortida.append(x.strip())# elimina os espaços e adiciona na lista
-    arquivo.close()# fecha a leitura do arquivo
-
-    posicao = random.randrange(0,len(palavraSortida))# gera um numero aleatório com base na quantidade de palavras
-
-    cont = len(palavraSortida[posicao])
-    lista = ["_" for cont in palavraSortida[posicao]] # inicia uma lista com o caracter "_" para cada letra da palavra_secreta
-
+    palavraSecreta = sorteio.sortear()
+    cont = len(palavraSecreta)
+    lista = ["_" for cont in palavraSecreta] # inicia uma lista com o caracter "_" para cada letra da palavra_secreta
     print('Forca -> ', lista) #mostra a lista a ser completada
 
     while (not enforcou and not acertou):
         index = 0
         chute = input("informe um letra? ")
-        if(chute.lower() in palavraSortida[posicao]):
-            for x in palavraSortida[posicao]:
+        if(chute.lower() in palavraSecreta):
+            for x in palavraSecreta:
                 if(chute.lower() == x): #valida se existe a letra digitada mesmo sendo maiuscula
                     lista[index] = chute #coloca a letra achada na posição certa
                     acerto+=1 #soma os acertos
--- sorteio.py
+++ sorteio.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 def sortear():
     arquivo = open('palavras.txt', 'r')  # abra o arquivo.tx com as palavras a ser escolhidas
@@ -7,9 +7,8 @@ def sortear():
     for x in arquivo:  # peca o conteudo do arquivo e coloca dentro da palavraSortida
         palavraSortida.append(x.strip())  # elimina os espaços e adiciona na lista
     arquivo.close()  # fecha a leitura do arquivo
-
     posicao = random.randrange(0, len(palavraSortida))  # gera um numero aleatório com base na quantidade de palavras
-    print(palavraSortida[posicao])
+    return palavraSortida[posicao]
 
 if(__name__ == "__main__"):
     sortear()
